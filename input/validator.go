2d83cf81a0776e92848fc54e432e219e1a5b9285
Pavel Druzhinin <paveldroo@gmail.com>
2024-06-10 14:50:40 +0300

    validator + validator_test 100% done

--- input/validator.go
+++ input/validator.go
@@ -2,24 +2,36 @@ package input
 
 import "fmt"
 
-type Validator struct{}
+type Validator struct {
+	expectedLength int
+	validOperators []string
+}
 
-func NewValidator() *Validator {
-	return &Validator{}
+func NewValidator(len int, validOps []string) *Validator {
+	return &Validator{
+		expectedLength: len,
+		validOperators: validOps,
+	}
 }
 
 func (v *Validator) CheckInput(op string, operands []float64) error {
-	if len(op) != 1 {
-		return fmt.Errorf("bad operator")
+	if len(operands) != v.expectedLength {
+		return fmt.Errorf("invalid operands qiantity: %v", operands)
 	}
 
-	if op != "+" && op != "-" && op != "/" && op != "*" {
-		return fmt.Errorf("bad operator")
-	}
-
-	if len(operands) != 2 {
-		return fmt.Errorf("wrong quantity of operands")
+	err := v.CheckOperator(op)
+	if err != nil {
+		return err
 	}
 
 	return nil
+}
+
+func (v *Validator) CheckOperator(op string) error {
+	for _, vops := range v.validOperators {
+		if vops == op {
+			return nil
+		}
+	}
+	return fmt.Errorf("invalid operator: %v", op)
 }
---
+++ input/validator_test.go
@@ -0,0 +1,57 @@
+package input_test
+
+import (
+	"github.com/paveldroo/tdd-in-go-book/input"
+	"testing"
+)
+
+func TestValidator_CheckInput(t *testing.T) {
+	t.Run("valid input", func(t *testing.T) {
+		// Arrange
+		validator := setup(t, []string{"+"})
+		operator := "+"
+		operands := []float64{2.0, 3.0}
+
+		// Act
+		err := validator.CheckInput(operator, operands)
+
+		// Assert
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+	t.Run("invalid operands quantity", func(t *testing.T) {
+		// Arrange
+		validator := setup(t, []string{"+"})
+		operator := "+"
+		operands := []float64{3.0}
+
+		// Act
+		err := validator.CheckInput(operator, operands)
+
+		// Assert
+		if err == nil {
+			t.Fatal(err)
+		}
+	})
+	t.Run("invalid operator", func(t *testing.T) {
+		// Arrange
+		validator := setup(t, []string{"+"})
+		operator := "-"
+		operands := []float64{2.0, 3.0}
+
+		// Act
+		err := validator.CheckInput(operator, operands)
+
+		// Assert
+		if err == nil {
+			t.Fatal(err)
+		}
+	})
+
+}
+
+func setup(t *testing.T, validOps []string) *input.Validator {
+	t.Helper()
+	return input.NewValidator(2, validOps)
+}
